7aca805c5f88f653c446287385314f3039dabcab
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-02-23 21:14:13 +0200

    Removed intercept

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -5,8 +5,8 @@ class CfgPatches {
 		name = "Beacon";
 		units[] = {};
 		weapons[] = {};
-		requiredVersion = 1.88;
-		requiredAddons[] = {"intercept_core"};
+		requiredVersion = 1.96;
+		requiredAddons[] = {"cba_main"};
 		author = "CreepPork";
 		authors[] = {"CreepPork"};
 		url = "https://github.com/CreepPork/Beacon";
@@ -14,14 +14,6 @@ class CfgPatches {
 		versionStr = "1.0";
 		versionAr[] = {1,0};
 	};
-};
-
-class Intercept {
-    class CreepPork {
-        class Beacon {
-            pluginName = "beacon";
-        };
-    };
 };
 
 class Extended_PreStart_EventHandlers {
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,3 @@
-extern crate libc;
-
-#[link(name = "intercept")]
-extern "cdecl" {
-    type intercept {
-        fn api_version() -> u32;
-        fn on_frame();
-        fn post_init();
-        fn mission_ended();
-    }
-}
-
-fn post_init() {
-    intercept::api_version();
+fn main() {
+    //
 }
