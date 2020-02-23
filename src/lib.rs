e83e747f7d82978ad793e4cc9787ff9b8de1d64e
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-02-23 16:53:26 +0200

    Added .env support

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+extern crate dotenv;
+
 use arma_rs::{rv, rv_handler};
+use dotenv::dotenv;
+use std::env;
 
 #[rv]
 fn hello() -> &'static str {
@@ -11,4 +15,6 @@ fn is_arma3(version: u8) -> bool {
 }
 
 #[rv_handler]
-fn init() {}
+fn init() {
+    dotenv().ok();
+}
