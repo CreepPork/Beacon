42573e19c266073a698428216e1b3c9bb6342214
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-03-30 18:18:27 +0300

    Added mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "Beacon 1.0.0";
+actionName = "GitHub";
+action = "https://github.com/CreepPork/Beacon";
+description = "Beacon - Version 1.0.0";
+tooltip = "Beacon";
+tooltipOwned = "Beacon Owned";
+overview = "A Rust library which is loaded by Arma 3, as an extension, to output game data to a Laravel app.";
+author = "CreepPork";
